34ae23c26fdd3badf8ec11352418daaa9bd2c98e
AndresPiola <67182081+AndresPiola@users.noreply.github.com>
2021-06-17 11:01:12 -0400

    asd

--- Assets/Scripts/Sesion9/EnemyShip.cs
+++ Assets/Scripts/Sesion9/EnemyShip.cs
@@ -8,13 +8,15 @@ namespace Shmup
     public class EnemyShip : MonoBehaviour
     {
         public float fireRate = 1f;
+
+        public int circleBulletAmmount = 20;
         // Start is called before the first frame update
         void Start()
         {
-            StartCoroutine(Shoot());
+            StartCoroutine(ShootCircle());
         }
 
-        IEnumerator Shoot()
+        IEnumerator ShootForward()
         {
             GameObject bullet;
             while(true)
@@ -27,5 +29,39 @@ namespace Shmup
             }
 
         }
+
+       
+        IEnumerator ShootCircle()
+        {
+
+            Vector2 ShootDir = transform.up;
+ 
+            GameObject bullet;
+            
+            while (true)
+            {
+                for (int i = 0; i < circleBulletAmmount; i++)
+                {
+                    bullet = EnemyBulletPool.Instance.GetPooledObject(transform.position);
+                    bullet.GetComponent<EnemyBullet>().ShootTo(Rotate2D(transform.up, Mathf.Deg2Rad * 360 / circleBulletAmmount *i));
+
+                }
+
+                yield return new WaitForSeconds(fireRate);
+
+            }
+
+        }
+
+        Vector2 Rotate2D(Vector2 vector, float angle)
+        {
+            Vector2 rotatedVector = Vector2.zero;
+            rotatedVector.x = (vector.x * Mathf.Cos(angle)) - (vector.y * Mathf.Sin(angle));
+            rotatedVector.y = (vector.x * Mathf.Sin(angle)) + (vector.y * Mathf.Cos(angle));
+
+
+            return rotatedVector;
+        }
+
     }
 }
